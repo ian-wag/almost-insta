060f29a3dcfb031a73b923fc6aa7d886f2620e92
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2021-10-12 12:25:28 -0500

    search bar styling

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,19 @@
 import Image from 'next/image';
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIco,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from '@heroicons/react/solid';
 
 const Header = () => {
   return (
     <div>
       <div className="flex justify-between max-w-6xl">
         {/* LEFT  */}
-        <div className="relative hidden lg:inline-grid h-24 w-24">
+        <div className="relative hidden lg:inline-grid w-24">
           <Image
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2880px-Instagram_logo.svg.png"
             layout="fill"
@@ -13,14 +21,24 @@ const Header = () => {
           />
         </div>
 
-        <div className="relative w-10 h-10 lg:hidden flex-shrink-0">
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
           <Image
             src="https://1000logos.net/wp-content/uploads/2017/02/insta-logo.png"
             layout="fill"
             objectFit="contain"
           />
         </div>
-        {/* MIDDLE  */}
+        {/* MIDDLE - SEARCH */}
+        <div className="relative mt-1 p-3 rounded-md">
+          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+            <SearchIcon className="h-5 w-5 text-gray-400" />
+          </div>
+          <input
+            className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
         {/* RIGHT  */}
       </div>
     </div>
