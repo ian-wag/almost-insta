eed88c1578e36459225df88a55553de8b227a9cc
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2021-10-13 13:43:18 -0500

    changed search bar on mobile to not cut off icon

--- components/Header.js
+++ components/Header.js
@@ -31,13 +31,13 @@ const Header = () => {
         </div>
 
         {/* MIDDLE - SEARCH */}
-        <div className="max-w-xs">
+        <div className="max-w-[12rem] sm:max-w-xs">
           <div className="relative mt-1 p-3 rounded-md">
             <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
               <SearchIcon className="h-5 w-5 text-gray-400" />
             </div>
             <input
-              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+              className="bg-gray-50 block w-full pl-10 text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
               type="text"
               placeholder="Search"
             />
