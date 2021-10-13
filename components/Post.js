c8c4b793c49da07b2d0fa4b209ebee98b52bc51e
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2021-10-13 17:22:07 -0500

    started post and posts components

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,34 @@
-function Post() {
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from '@heroicons/react/outline';
+import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid';
+
+function Post({ id, username, userImg, img, caption }) {
   return (
-    <div>
-      <h1>POST ALERT</h1>
+    <div className="bg-white my-7 border rounded-sm">
+      {/* HEADER */}
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+          alt="user profile pic"
+        />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+
+      {/* IMAGE */}
+      <img src={img} className="object-cover w-full" alt="post image" />
+
+      {/* BUTTONS */}
+      {/* CAPTION */}
+      {/* COMMENTS */}
+      {/* INPUT BOX */}
     </div>
   );
 }
--- components/Posts.js
+++ components/Posts.js
@@ -1,9 +1,37 @@
 import Post from './Post';
 
+const posts = [
+  {
+    id: '123',
+    username: 'mbn',
+    userImg:
+      'https://avatars.githubusercontent.com/u/64107817?s=400&u=89b23fddfe3b1006b3d51c2f38fd3eebaf56dc07&v=4',
+    img: 'https://pbs.twimg.com/media/E-YEPflXICsKbuQ?format=jpg&name=large',
+    caption: 'we out here.',
+  },
+  {
+    id: '123',
+    username: 'mbn',
+    userImg:
+      'https://avatars.githubusercontent.com/u/64107817?s=400&u=89b23fddfe3b1006b3d51c2f38fd3eebaf56dc07&v=4',
+    img: 'https://pbs.twimg.com/media/E-YEPflXICsKbuQ?format=jpg&name=large',
+    caption: 'we out here.',
+  },
+];
+
 function Posts() {
   return (
     <div>
-      <Post />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.username}
+          userImg={post.userImg}
+          img={post.img}
+          caption={post.caption}
+        />
+      ))}
     </div>
   );
 }
