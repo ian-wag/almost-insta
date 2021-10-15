961649f422aec9e6954ba0d2e81b5c935f47f79e
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2021-10-15 14:10:29 -0500

    sign in page style and route back after sign in

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,15 +1,33 @@
 import { getProviders, signIn as SignIntoProvider } from 'next-auth/react';
+import Header from '../../components/Header';
 
 function signIn({ providers }) {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => SignIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 mt-56 px-14 text-center">
+        <img
+          className="w-80"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2880px-Instagram_logo.svg.png"
+          alt="instagram logo"
+        />
+        <p className="font-xs italic">
+          Not the REAL INSTAGRAM. This website was built for practice only!
+        </p>
+
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="p-3 bg-blue-500 rounded-lg text-white"
+                onClick={() => SignIntoProvider(provider.id, { callbackUrl: '/' })}
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 }
