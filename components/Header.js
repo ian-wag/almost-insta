ed01dbabe3e695ab00dbb778a9b5b8b40f0b4619
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2021-10-18 14:57:43 -0500

    added recoil

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,12 @@ import {
 import { HomeIcon } from '@heroicons/react/solid';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/dist/client/router';
+import { useRecoilState } from 'recoil';
+import { modalState } from '../atoms/modalAtom';
 
 const Header = () => {
   const { data: session } = useSession();
+  const [open, setOpen] = useRecoilState(modalState);
   const router = useRouter();
 
   return (
@@ -67,7 +70,7 @@ const Header = () => {
                   3
                 </div>
               </div>
-              <PlusCircleIcon className="navBtn" />
+              <PlusCircleIcon onClick={() => setOpen(true)} className="navBtn" />
               <UserGroupIcon className="navBtn" />
               <HeartIcon className="navBtn" />
 
