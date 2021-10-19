87d7f97bb92bd830558d18f6e9df1445e1e54347
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2021-10-19 15:52:41 -0500

    added likes functionality

--- components/Post.js
+++ components/Post.js
@@ -1,10 +1,13 @@
 import {
   addDoc,
   collection,
+  deleteDoc,
+  doc,
   onSnapshot,
   orderBy,
   query,
   serverTimestamp,
+  setDoc,
 } from '@firebase/firestore';
 import {
   BookmarkIcon,
@@ -25,6 +28,7 @@ function Post({ id, username, userImg, img, caption }) {
   const [comment, setComment] = useState('');
   const [comments, setComments] = useState([]);
   const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
 
   useEffect(
     () =>
@@ -34,6 +38,26 @@ function Post({ id, username, userImg, img, caption }) {
       ),
     []
   );
+
+  useEffect(
+    () => onSnapshot(collection(db, 'posts', id, 'likes'), (snapshot) => setLikes(snapshot.docs)),
+    [db, id]
+  );
+
+  useEffect(
+    () => setHasLiked(likes.findIndex((like) => like.id === session?.user?.uid) !== -1),
+    [likes]
+  );
+
+  const likePost = async (e) => {
+    if (hasLiked) {
+      await deleteDoc(doc(db, 'posts', id, 'likes', session.user.uid));
+    } else {
+      await setDoc(doc(db, 'posts', id, 'likes', session.user.uid), {
+        username: session.user.username,
+      });
+    }
+  };
 
   const sendComment = async (e) => {
     e.preventDefault();
@@ -69,7 +93,11 @@ function Post({ id, username, userImg, img, caption }) {
       {session && (
         <div className="flex justify-between px-4 pt-4">
           <div className="flex space-x-4 ">
-            <HeartIcon className="btn" />
+            {hasLiked ? (
+              <HeartIconFilled onClick={likePost} className="btn text-red-500" />
+            ) : (
+              <HeartIcon onClick={likePost} className="btn" />
+            )}
             <ChatIcon className="btn" />
             <PaperAirplaneIcon className="btn rotate-45" />
           </div>
@@ -79,6 +107,7 @@ function Post({ id, username, userImg, img, caption }) {
 
       {/* CAPTION */}
       <p className="p-5 truncate">
+        {likes.length > 0 && <p className="font-bold mb-1">{likes.length} likes</p>}
         <span className="font-bold mr-1">{username} </span>
         {caption}
       </p>
